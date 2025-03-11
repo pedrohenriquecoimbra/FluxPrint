787080f6876d4885cde34bbae9c827d5cf0533a8
Pedro Henrique H. Coimbra <49572701+pedrohenriquecoimbra@users.noreply.github.com>
2025-03-11 18:01:39 +0100

    ignore if Kljun et al. (2015) model not available

--- footprint_tools/model/ffp_kljun2015.py
+++ footprint_tools/model/ffp_kljun2015.py
@@ -1,1 +1,6 @@
-from ffpkljun import *
+try:
+    from ffpkljun import *
+except ModuleNotFoundError:
+    print("ModuleNotFoundError: No module named 'ffpkljun'. Use 'pip install' or please contact moderator.")
+    get_contour_levels = None
+    get_contour_vertices = None
